a090e476a9a31d6a003af82abf5361489484dfa4
TomxHWRxWI20C <84907054+TomxHWRxWI20C@users.noreply.github.com>
2022-03-31 10:56:10 +0200

    removed joda.time, revisite Time, implemented change requests

--- src/main/java/com/zeiterfassung/hwr/desktop/DesktopApplication.java
+++ src/main/java/com/zeiterfassung/hwr/desktop/DesktopApplication.java
@@ -2,8 +2,6 @@ package com.zeiterfassung.hwr.desktop;
 
 import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
 
 
 @SpringBootApplication
--- src/main/java/com/zeiterfassung/hwr/desktop/entities/Time.java
+++ src/main/java/com/zeiterfassung/hwr/desktop/entities/Time.java
@@ -1,13 +1,11 @@
 package com.zeiterfassung.hwr.desktop.entities;
 
 import lombok.Data;
-import org.joda.time.DateTime;
 
 @Data
 public class Time
 {
-        private DateTime start;
-        private DateTime end;
+        private boolean isStart;
         private boolean isBreak;
-        private Project project;
+        private int projectID;
 }
