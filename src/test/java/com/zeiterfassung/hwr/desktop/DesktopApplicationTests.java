f1e767346a78f1563ed3661b94c2c135cce93334
TomxHWRxWI20C <84907054+TomxHWRxWI20C@users.noreply.github.com>
2022-04-04 01:03:13 +0200

    removed JavaFx components from Beans, implemented TestFX

--- src/test/java/com/zeiterfassung/hwr/desktop/DesktopApplicationTests.java
+++ src/test/java/com/zeiterfassung/hwr/desktop/DesktopApplicationTests.java
@@ -1,13 +1,24 @@
 package com.zeiterfassung.hwr.desktop;
 
+import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
 
-@SpringBootTest
-class DesktopApplicationTests {
+@ExtendWith(ApplicationExtension.class)
+class DesktopApplicationTests
+{
 
-	@Test
-	void contextLoads() {
-	}
+
+    @Start
+    private void start(Stage stage)
+    {
+    }
+
+    @Test
+    void contextLoads()
+    {
+    }
 
 }
