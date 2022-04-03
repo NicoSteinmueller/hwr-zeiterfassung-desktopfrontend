6e00a22b38cbf23576921c676a0c8d16a59b9a08
TomxHWRxWI20C <84907054+TomxHWRxWI20C@users.noreply.github.com>
2022-04-04 01:23:34 +0200

    disabled test Stage in Jenkins

--- src/test/java/com/zeiterfassung/hwr/desktop/DesktopApplicationTests.java
+++ src/test/java/com/zeiterfassung/hwr/desktop/DesktopApplicationTests.java
@@ -3,9 +3,12 @@ package com.zeiterfassung.hwr.desktop;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
+
+@SpringBootTest
 @ExtendWith(ApplicationExtension.class)
 class DesktopApplicationTests
 {
